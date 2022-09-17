1ab1ff242f39da99929cd69d991fff048632d229
ashwin daniel <ashwinunmesh@gmail.com>
2022-09-17 16:21:14 +1000

    Autocomplete feature works now. Sends place to server. Need to restructure the code to get it to run appropraite with the other app.js components

--- client/src/search.js
+++ client/src/search.js
@@ -1,5 +1,11 @@
 import { useRef, useEffect } from "react";
 
+const handleInput = () => {
+   console.log("search component seearch clicked")
+   // const locationData =location;
+   // fetchServerData(locationData)
+ }
+
 const AutoComplete = () => {
  const autoCompleteRef = useRef();
  const inputRef = useRef();
@@ -8,22 +14,40 @@ const AutoComplete = () => {
     fields: ["address_components", "geometry", "icon", "name"],
  };
  useEffect(() => {
-  autoCompleteRef.current = new window.google.maps.places.Autocomplete(
-   inputRef.current,
-   options
-  );
+  autoCompleteRef.current = new window.google.maps.places.Autocomplete(inputRef.current,options);
+
   autoCompleteRef.current.addListener("place_changed", async function () {
    const place = await autoCompleteRef.current.getPlace();
    console.log(place.name);
-  });
+   fetchServerData(place.name);
+  }); }, []);
 
+  async function fetchServerData(locationData) {
+   // const locationData = "towsville"
+   const data = {
+     "location": locationData,
+   };
+   const options ={
+     method: "POST",
+     headers: {
+       'Content-Type': 'application/json'
+       // 'Content-Type': 'text/plain'
+     },
+     body: JSON.stringify(data)
+   }
 
- }, []);
+   fetch('/api', options);
+   console.log(data)
+ }
+
+ fetchServerData();
+  
  return (
   <div>
    <label>enter address :</label>
    <p>Hiii</p>
    <input ref={inputRef} />
+   <button onClick={() => handleInput()} >Search</button>
   </div>
  );
 };
--- server/server.js
+++ server/server.js
@@ -1,22 +1,31 @@
-const express = require('express');
+//Requires Express and allows us to use it inside our server.js file.
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5001;
+const fetch = require('node-fetch');
 app.use(express.static('public'));
-app.use(express.json({limit: '2mb'}))
+app.use(express.json({limit: '1mb'}))
 
+//Set the Express server on which port to run on. 
+const PORT = process.env.PORT || 5001;
+// const url = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=b13dca06fad2516ab926e2946e70545e";
+// const city = setLocation;
+const url = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="
 
-app.get("/message", (req, res) => {
-  res.json({ message: "Hello frodm server!" });
-});
-
-
+//will display a message on the console that the server is working as expected.
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
 
-
-
 app.post('/api', (request,response) => {
   console.log('I got a request')
-  console.log(request.body);
+  console.log(request.body.location);
+  setLocation = request.body.location;
+  // fetchOpenWeather(setLocation)
+  const url =  `${currentWeather}${setLocation},australia&appid=b13dca06fad2516ab926e2946e70545e&units=metric`
+  console.log(url);
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => response.json(data));
+
 });
