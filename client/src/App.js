fb2d4dd677067798906769afcc1a61b1db14b4a3
ashwin daniel <ashwinunmesh@gmail.com>
2022-10-09 22:18:27 +1000

    scroll animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-import React,{useState} from "react";
-import AutoComplete from './search.js';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import AutoComplete from "./search.js";
 
 function App() {
   const [data, setData] = React.useState(null);
   const [location, setLocation] = useState(null);
-  const [word, setWord] = useState('word usestate');
+  const [word, setWord] = useState("word usestate");
 
   const handleInput = (props) => {
     const locationData = location;
     // console.log(props.location);
-    fetchServerData(locationData)
-  }
+    fetchServerData(locationData);
+  };
 
   React.useEffect(() => {
     fetch("/message")
       .then((res) => res.json())
-      .then((data) => setData(data.message))
+      .then((data) => setData(data.message));
   }, []);
 
-  console.log(data)
-  
+  console.log(data);
 
   async function fetchServerData(locationData) {
     // const locationData = "towsville"
     const data = {
-      "location": word,
+      location: word,
     };
-    const options ={
+    const options = {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
         // 'Content-Type': 'text/plain'
       },
-      body: JSON.stringify(data)
-    }
+      body: JSON.stringify(data),
+    };
 
-    fetch('/api', options);
-    console.log(data)
+    fetch("/api", options);
+    console.log(data);
   }
 
   fetchServerData();
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <AutoComplete
-         sendLocation={location => setWord(location)}/>
-        <input placeholder="location..." onChange={e => setLocation(e.target.value)}></input>
-        <button onClick={() => handleInput()} >Search</button>
-        <button></button>
-        <h1>{word}</h1>
-      </header>
+      <div className="container-scroll y mandatory-scroll-snapping">
+        <section className="test">Y Mand. LTR</section>
+
+        <section className="test">2</section>
+      </div>
     </div>
   );
 }
